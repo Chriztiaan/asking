34bec9718f3bcd9bf8c4aa341d1df8add9f3c733
Christiaan Landman <chriz.ek@gmail.com>
2022-09-19 21:34:11 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,9 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: ['~/components']
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
